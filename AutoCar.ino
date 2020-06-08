4ab475c8a25022f0ca988d72ee3ad0efc9ec2401
terhead8775 <jack88677@gmail.com>
2020-06-08 16:40:46 +0800

    建立前進後退函數

--- AutoCar.ino
+++ AutoCar.ino
@@ -1,49 +1,36 @@
 #include <AFMotor.h>
 
-// Connect a stepper motor with 64 steps per revolution (5.625 degree)
-// to motor port #2 (M3 and M4) 
 AF_Stepper left(256, 1);
-AF_Stepper right(16, 2);
+AF_Stepper right(256, 2);
 
-int microStep = 0;
+int start = 0;
 boolean bothDirection = true;
 
-//gunsigned int speed = 2000;    // 2000make a modification for your test
+void forward(){
+  left.onestep(BACKWARD, DOUBLE );
+  right.onestep(FORWARD, DOUBLE );
+}
 
-//unsigned long int ltime = 0, rtime = 0;
+void backward(){
+  left.onestep(FORWARD, DOUBLE );
+  right.onestep(BACKWARD, DOUBLE );
+}
 
 void setup() {
-  Serial.begin(9600);         // set up Serial library at 9600 bps
+  Serial.begin(9600);         
   Serial.setTimeout(100);
   Serial.println("Stepper test!");
- // left.setSpeed(speed);
-  //right.setSpeed(speed);
 }
+
 void loop() {
-//  Serial.print("ms:");
-//  Serial.println(microStep);
+
   if(Serial.available()) {
-    microStep = Serial.parseInt();
+    start = Serial.parseInt();
   }
   
-  if (microStep==1)
+  if (start==1)
   {
-//    Serial.println("Micrsostep steps");
-    //unsigned long int currentTime = millis();
-    //if(currentTime-ltime>1) {
-      left.onestep(BACKWARD, DOUBLE );
-      right.onestep(FORWARD, DOUBLE );
+      backward();
       delay(2);
-    //  ltime = currentTime;
-    //}
-    //if(currentTime-rtime>1) {
-    //  right.onestep(FORWARD, DOUBLE    );
-    //  rtime = currentTime;
-    //}
-//    left.step(2048, FORWARD, MICROSTEP);
-//    right.step(2048, FORWARD, MICROSTEP);
-//    if (bothDirection){
-//      left.step(2048, BACKWARD, MICROSTEP);
-//      right.step(2048, BACKWARD, MICROSTEP);}
   }
 }
