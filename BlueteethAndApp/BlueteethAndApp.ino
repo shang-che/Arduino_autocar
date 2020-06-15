7e574084e60a0d81e31b040642eecbe4ece78841
PROcreeter <creeter1719@gmail.com>
2020-06-15 16:29:58 +0800

    arduino超音波完成(成功通訊) APP完成超音波顯示，新增開始與強制停止按鈕，新增x,y,狀態，刪除轉速

--- BlueteethAndApp/BlueteethAndApp.ino
+++ BlueteethAndApp/BlueteethAndApp.ino
@@ -1,16 +1,16 @@
 #include <SoftwareSerial.h>
 
-const int rxpin = 2;
-const int txpin = 3;
+const int rxpin = 3;
+const int txpin = 2;
 SoftwareSerial bluetooth(rxpin, txpin); 
 
 //F前 L左 R右
-int TrigPinF = A0;
-int EchoPinF = A1;
-int TrigPinL = A2;
-int EchoPinL = A3;
-int TrigPinR = A4;
-int EchoPinR = A5;
+int TrigPinF = A4;
+int EchoPinF = A5;
+int TrigPinL = A0;
+int EchoPinL = A1;
+int TrigPinR = A2;
+int EchoPinR = A3;
 
 unsigned long getDistance(int TrigPin,int EchoPin) {
     digitalWrite(TrigPin, LOW);
@@ -42,20 +42,21 @@ void loop() {
   //在監控面顯示距離
     unsigned long distanceF = getDistance(TrigPinF,EchoPinF);
     Serial.println(distanceF);
+    delay(100);
     unsigned long distanceL = getDistance(TrigPinL,EchoPinL);
     Serial.println(distanceL);
     unsigned long distanceR = getDistance(TrigPinR,EchoPinR);
     Serial.println(distanceR);
     Serial.println("---------------");
     
-    //在app顯示距離
+
     // 用 @ 做為開始字元
     bluetooth.print("@"); 
-    bluetooth.print("11");
-    bluetooth.write(13); 
-    bluetooth.print("22"); 
-    bluetooth.write(13);   
-    bluetooth.print("33");
+    bluetooth.print(distanceF);
+    bluetooth.print(" "); 
+    bluetooth.print(distanceL); 
+    bluetooth.print(" ");   
+    bluetooth.print(distanceR);
     bluetooth.write(13); 
     
     // 用 ASCII碼 13(F)做為結束字元
