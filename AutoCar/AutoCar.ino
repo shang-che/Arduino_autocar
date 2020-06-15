c82704182ffdaaf3cda05f4d74362deff7c46e65
terhead8775 <jack88677@gmail.com>
2020-06-15 16:49:40 +0800

    車體個零件測試完成 可開始串寫演算法

--- AutoCar/AutoCar.ino
+++ AutoCar/AutoCar.ino
@@ -1,43 +1,91 @@
 #include <AFMotor.h>
 
-AF_Stepper left(256, 1);
+#define LeftTrig A0 //定義超音波感測器腳位
+#define LeftEcho A1
+#define RightTrig A2
+#define RightEcho A3
+#define FrontTrig A4
+#define FrontEcho A5
+
+#define sensor 9 //定義紅外線感測器腳位
+
+#define RXD 2 //定義藍芽腳位
+#define TXD 10
+
+AF_Stepper left(256, 1); //宣告步進馬達腳位
 AF_Stepper right(256, 2);
-int sensor = 9;
-boolean sensorstate = 0;
 
-int start = 0;
+boolean sensorstate = 0; //宣告感應器狀態
 
+boolean start = 0; //宣告開始變數
+
+//定義前進函數
 void forward(){
   left.onestep(BACKWARD, DOUBLE );
   right.onestep(FORWARD, DOUBLE );
 }
 
+//定義後退函數
 void backward(){
   left.onestep(FORWARD, DOUBLE );
   right.onestep(BACKWARD, DOUBLE );
 }
 
+//定義右轉函數
+void rightward(){
+  left.onestep(BACKWARD, DOUBLE );
+  right.onestep(BACKWARD, DOUBLE );
+}
+
+//定義左轉函數
+void leftward(){
+  left.onestep(FORWARD, DOUBLE );
+  right.onestep(FORWARD, DOUBLE );
+}
+
+unsigned long GetDistance(int Trig, int Echo) {
+  unsigned long distance=100;
+  digitalWrite(Trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trig, LOW);
+  unsigned long duration = pulseIn(Echo, HIGH);
+  distance = duration/58;
+  delay(100);
+  return distance;
+}
+
+
 void setup() {
   Serial.begin(9600);         
-  Serial.setTimeout(100);
-  Serial.println("Stepper test!");
+  Serial.setTimeout(100); //每100毫秒接收一次
+  Serial.println("test start!");
   pinMode(sensor, INPUT);
+  pinMode(LeftTrig, OUTPUT);
+  pinMode(LeftEcho, INPUT);
+  pinMode(RightTrig, OUTPUT);
+  pinMode(RightEcho, INPUT);
+  pinMode(FrontTrig, OUTPUT);
+  pinMode(FrontEcho, INPUT);
 }
 
 void loop() {
+  if (start==1 )
+  {
+
+  }
   sensorstate = digitalRead(sensor);
-  if(sensorstate == 0)
-    Serial.println("clear");
-  else
-    Serial.println("line");
-  delay(500);
+ // if(sensorstate == 0)
+ //  Serial.println("clear");
+ // else
+ //   Serial.println("line");
+    
+ // delay(500);
+  
   if(Serial.available()) {
     start = Serial.parseInt();
   }
-  if (start==1)
-  {
-      forward();
-      delay(2);
-  }
+  
   
 }
