2874d3f94b7dd8c4f3814bd65ab0f08a697b0065
PROcreeter <creeter1719@gmail.com>
2020-07-03 07:54:39 +0800

    整理

--- motorcontrol/motorcontrol.ino
+++ motorcontrol/motorcontrol.ino
@@ -11,26 +11,23 @@ int valueRB = 0;//右馬達控制變數
 char stats='X';
 //A5 LOW 在做動作，無視上層指令
 //A5 HIGH 接受上層指令
+
 //定義前進函數
 void forward(){
-  //Serial.println("f");
   stats='f';
   for(int i = 0;i<50;i++){
     left.onestep(BACKWARD, DOUBLE );
     right.onestep(FORWARD, DOUBLE );
     delay(2);
   }
-  //digitalWrite(A5, HIGH);
 }
 
-//接受上層指令
 //定義右轉函數
 void rightward(){
   if(stats!='R'){
     stats='R';
     digitalWrite(A5, LOW);
-    Serial.println("startr");
-    for(int i = 0;i<1400;i++){
+    for(int i = 0;i<1400;i++){//可能要改----------
       left.onestep(BACKWARD, DOUBLE );
       right.onestep(FORWARD, DOUBLE );
       delay(2);
@@ -40,14 +37,13 @@ void rightward(){
       right.onestep(BACKWARD, DOUBLE );
       delay(2);
     }
-    for(int k = 0;k<600;k++){
+    for(int k = 0;k<700;k++){//可能要改----------
       left.onestep(BACKWARD, DOUBLE );
       right.onestep(FORWARD, DOUBLE );
       delay(2);
     }
      
     digitalWrite(A5, HIGH);
-    Serial.println("endr");
   }
   else{
     valueLA=LOW;
@@ -59,9 +55,8 @@ void rightward(){
 void leftward(){//定義左轉函數
   if(stats!='L'){
     stats='L';
-    digitalWrite(A5, LOW);
-    Serial.println("startl");  
-    for(int i = 0;i<1400;i++){
+    digitalWrite(A5, LOW); 
+    for(int i = 0;i<1400;i++){//可能要改----------
       left.onestep(BACKWARD, DOUBLE );
       right.onestep(FORWARD, DOUBLE );
       delay(2);
@@ -72,14 +67,13 @@ void leftward(){//定義左轉函數
       right.onestep(FORWARD, DOUBLE );
       delay(2);
     }
-    for(int k = 0;k<700;k++){
+    for(int k = 0;k<700;k++){//可能要改----------
       left.onestep(BACKWARD, DOUBLE );
       right.onestep(FORWARD, DOUBLE );
       delay(2);
     }
     digitalWrite(A5, HIGH);
     stopmove();
-    Serial.println("endl"); 
   }
   else{
     valueLA=LOW;
@@ -92,54 +86,33 @@ void leftward(){//定義左轉函數
 //定義右修正函數
 void rf(){
     stats='r';
-    //digitalWrite(A5, LOW);
     Serial.println("rf");  
     left.onestep(BACKWARD, DOUBLE );
     right.onestep(FORWARD, DOUBLE );
     delay(2);
     left.onestep(BACKWARD, DOUBLE );
     right.onestep(BACKWARD, DOUBLE );
-    delay(2);
-    //digitalWrite(A5, HIGH);  
+    delay(2);  
 }
 
-void lf(){
-    //digitalWrite(A5, LOW); 
+void lf(){ 
     Serial.println("lf"); 
     left.onestep(BACKWARD, DOUBLE );
     right.onestep(FORWARD, DOUBLE );
     delay(2);
     left.onestep(FORWARD, DOUBLE );
     right.onestep(FORWARD, DOUBLE );
-    delay(2);
-    //digitalWrite(A5, HIGH);    
+    delay(2);    
 }
 void stopmove(){  
   Serial.println("stop");
 }
 
-void setup() {
-  Serial.begin(9600);         
-  Serial.setTimeout(100); //每100毫秒接收一次
-  pinMode(A0, INPUT);//LA
-  pinMode(A1, INPUT);//LB
-  pinMode(A2, INPUT);//RA
-  pinMode(A3, INPUT);//RB
-  pinMode(A5, OUTPUT);//通訊上層
-  digitalWrite(A5,HIGH);
-  delay(5000);
-}
-
-void loop() {
-  
-  valueLA = digitalRead(A0);
-  //delay(100);
-  valueLB = digitalRead(A1);
- // delay(100);
+void getValue(){//接收訊號/電腦監控訊號);
+  valueLB = digitalRead(A0);
+  valueLA = digitalRead(A1);
   valueRA = digitalRead(A2);
- // delay(100);
   valueRB = digitalRead(A3);
- // delay(100);
   Serial.print("LA");
   Serial.println(valueLA);
   Serial.print("LB");
@@ -150,23 +123,23 @@ void loop() {
   Serial.println(valueRB);
   Serial.print("stats;");
   Serial.println(stats);
-  //delay(300);
-  //控制方
+  Serial.println("---------------");
+}
+void runway(){//判斷訊號
   //前進:1111
-  //(沒有)後退:XXXX   
   //左修正:1000
   //右修正:0010
   //左轉:1100
   //右轉:0011
   //停止:0000
 
-  if(valueLA==1&&valueLB==1){
-    if(valueRA==1&&valueRB==1){//1111 
-      forward();
-    }
-    else{//1100
-      leftward();
-    }
+    if(valueLA==1&&valueLB==1){
+      if(valueRA==1&&valueRB==1){//1111 
+        forward();
+      }
+      else{//1100
+        leftward();
+      }
   }
   else if(valueLA==1&&valueLB==0){//1000
     lf();
@@ -182,7 +155,20 @@ void loop() {
       stopmove();
     }
   }
+}
+void setup() {
+  Serial.begin(9600);         
+  Serial.setTimeout(100); //每100毫秒接收一次
+  pinMode(A0, INPUT);//LA
+  pinMode(A1, INPUT);//LB
+  pinMode(A2, INPUT);//RA
+  pinMode(A3, INPUT);//RB
+  pinMode(A5, OUTPUT);//通訊上層
+  digitalWrite(A5,HIGH);
+  delay(5000);
+}
 
-  Serial.println("---------------");
-
+void loop() {
+  getValue();
+  runway();
 }
